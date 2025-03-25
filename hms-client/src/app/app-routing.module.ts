e1f9e54fbbf580973cdb3086362ca3e830c029df
nawsish <nawsish2018@gmail.com>
2025-03-25 13:39:25 +0600

    add update

--- hms-client/src/app/app-routing.module.ts
+++ hms-client/src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DoctorListComponent } from './frontEnd/doctor-list/doctor-list.component';
 import { DoctorComponent } from './doctor/doctor.component';
+import { NavbarComponent } from './frontEnd/navbar/navbar.component';
 
 const routes: Routes = [
+  { path: '', title:'Doctor Submit', component: NavbarComponent},
   { path: 'doctor', title:'Doctor Submit', component: DoctorComponent},
   { path: 'doctor-list',title:'Doctor List', component: DoctorListComponent }
 ];
